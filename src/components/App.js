9649ff524d1a5962774e81dee41823eb35c2aad5
18subrato <subratoverma7@gmail.com>
2025-04-21 15:58:16 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [whether, setWhether] = useState({ temperature: 25, conditions: "Sunny" })
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+
+      <li style={{ color: whether.temperature > 20 ? 'red' : 'blue' }}>{whether.temperature}</li>
+      <li>{whether.conditions}</li>
+
+    </div>)
 }
 
 export default App
