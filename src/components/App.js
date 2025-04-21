784a1b5af6de393f00e68bb814182777d2a37c56
18subrato <subratoverma7@gmail.com>
2025-04-21 16:07:31 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,11 @@ const App = () => {
   const [whether, setWhether] = useState({ temperature: 25, conditions: "Sunny" })
 
   return (
-    <div>
-
-     
-      <p style={{ color: whether.temperature > 20 ? 'red' : 'blue' }}> <span >{whether.temperature}</span>{whether.conditions}</p>
-
-    </div>)
+    <div style={{color:whether.temperature > 20 ? 'red':'blue'}}>
+      <p>temperature : {whether.temperature}</p>
+      <span>condition : {whether.conditions}</span> 
+    </div>
+    )
 }
 
 export default App
